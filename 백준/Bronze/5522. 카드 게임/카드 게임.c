b2a0be16beba6bb6d0cf2e67fb5a4313157a2625
jeong1010 <78248064+jeong1010@users.noreply.github.com>
2024-08-03 00:54:54 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int score[5];
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &score[i]);
+		sum += score[i];
+	}
+	printf("%d", sum);
+	return 0;
+}
