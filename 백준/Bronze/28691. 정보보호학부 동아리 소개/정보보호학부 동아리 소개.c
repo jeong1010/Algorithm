2c2c39c20296b6e8eb5b888e6d49502f2c8601c2
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2024-09-12 13:51:35 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+	char c;
+	scanf("%c", &c);
+	switch (c) {
+	case 'M':
+		printf("MatKor\n");
+		break;
+	case 'W':
+		printf("WiCys\n");
+		break;
+	case 'C':
+		printf("CyKor\n");
+		break;
+	case 'A':
+		printf("AlKor\n");
+		break;
+	case '$':
+		printf("$clear\n");
+		break;
+	}
+	return 0;
+}
