0d9bb1fb4163510040e3f76561e463553218bbdc
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2024-11-18 18:24:41 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/특정 위치의 문자 2/char-in-specific-location-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int c[10];
+    for(int i=0;i<10;i++)
+        scanf("%c ", &c[i]);
+    printf("%c %c %c\n", c[1], c[4], c[7]);
+    return 0;
+}
