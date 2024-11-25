8b66a3579c0b994e0afb2d090f46edb89db1ee43
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2024-11-25 17:12:29 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241125/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum=0;
+    for(int i=0;i<10;i++){
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("%d\n", sum);
+    return 0;
+}
