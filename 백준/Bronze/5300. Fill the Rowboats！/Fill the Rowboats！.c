fd15b89dd725c3552740cbb68c6db28287b0071f
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2025-04-13 11:19:15 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("%d ", i);
+		if (i % 6 == 0 || i == n)printf("Go! ");
+	}
+	printf("\n");
+	return 0;
+}
