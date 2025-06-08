e30a0336d1140e6ef2687fd648f2e7c1335edd3f
Jeongmin <78248064+jeong1010@users.noreply.github.com>
2025-06-08 16:23:15 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int a, b, c;
+	scanf("%d %d", &a, &b);
+	scanf("%d", &c);
+	if (a + b >= c*2) {
+		printf("%d\n", a + b - c*2);
+	}
+	else printf("%d\n", a + b);
+	return 0;
+}
